4201f0bd97dd8dc12cfec9dbe23ff692d9c49030
wangxiaoyang <wangxiaoyang@kingsoft.com>
2016-01-11 14:46:37 +0800

    1

--- src/main/java/com/seasun/data/datamining/ReadHive.java
+++ src/main/java/com/seasun/data/datamining/ReadHive.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URI;
+import java.net.URISyntaxException;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
@@ -15,8 +16,8 @@ import com.github.dataswitch.util.HadoopConfUtil;
 
 public class ReadHive {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws URISyntaxException {
+		new URI("hdfs:///");
         //String uri = "hdfs://xxxx:8020" + "/user/hive/warehouse/lib/col_iplib.txt";
 //        String uri = "hdfs://namenodem:50070/hive/warehouse/fig.db/lost_user/dt=2015-12-30/000000_0";
 //        String uri = "hdfs://namenodem:50070/hive/warehouse/fig.db/lost_user/dt=2015-12-30/000000_0";
@@ -25,8 +26,8 @@ public class ReadHive {
         FSDataInputStream in = null;
         BufferedReader br = null;
         try {
-//            fs = HadoopConfUtil.getFileSystem("hdfs", null);
-            fs = HadoopConfUtil.getFileSystem(null, null);
+            fs = HadoopConfUtil.getFileSystem("hdfs:///", null);
+//            fs = HadoopConfUtil.getFileSystem(null, null);
             in = fs.open(new Path(path));
             br = new BufferedReader(new InputStreamReader(in));
             String readline;
