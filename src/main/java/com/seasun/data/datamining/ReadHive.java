d1b50cbf3a9ffdc91118f6769918b4cdc21f8656
wangxiaoyang <wangxiaoyang@kingsoft.com>
2016-01-11 10:57:28 +0800

    read hive hdfs

---
+++ src/main/java/com/seasun/data/datamining/ReadHive.java
@@ -0,0 +1,43 @@
+package com.seasun.data.datamining;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
+
+import com.github.dataswitch.util.HadoopConfUtil;
+
+public class ReadHive {
+
+	public static void main(String[] args) {
+
+        //String uri = "hdfs://xxxx:8020" + "/user/hive/warehouse/lib/col_iplib.txt";
+        String uri = "hdfs://hive/warehouse/fig.db/lost_user/dt=2015-12-30/000000_0";
+        FileSystem fs = null;
+        FSDataInputStream in = null;
+        BufferedReader br = null;
+        try {
+            fs = HadoopConfUtil.getFileSystem(uri, "hadoop");
+            in = fs.open(new Path(uri));
+            br = new BufferedReader(new InputStreamReader(in));
+            String readline;
+            int counter = 0;
+            while ((readline = br.readLine()) != null && counter <=10) {
+                String[] pars = readline.split("\001");
+                System.out.println(pars[1]);
+                counter++;
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            IOUtils.closeStream(in);
+        }
+	}
+
+}
