3eef7329844bbae0af631073501e267ce1007742
wangxiaoyang <wangxiaoyang@kingsoft.com>
2016-01-11 14:40:38 +0800

    1

--- src/main/java/com/seasun/data/datamining/ReadHive.java
+++ src/main/java/com/seasun/data/datamining/ReadHive.java
@@ -18,13 +18,15 @@ public class ReadHive {
 	public static void main(String[] args) {
 
         //String uri = "hdfs://xxxx:8020" + "/user/hive/warehouse/lib/col_iplib.txt";
-        String uri = "hdfs://namenodem:50070/hive/warehouse/fig.db/lost_user/dt=2015-12-30/000000_0";
+//        String uri = "hdfs://namenodem:50070/hive/warehouse/fig.db/lost_user/dt=2015-12-30/000000_0";
+//        String uri = "hdfs://namenodem:50070/hive/warehouse/fig.db/lost_user/dt=2015-12-30/000000_0";
+		String path = "/hive/warehouse/fig.db/lost_user/dt=2015-12-30/000000_0";
         FileSystem fs = null;
         FSDataInputStream in = null;
         BufferedReader br = null;
         try {
-            fs = HadoopConfUtil.getFileSystem(uri, null);
-            in = fs.open(new Path(uri));
+            fs = HadoopConfUtil.getFileSystem(null, null);
+            in = fs.open(new Path(path));
             br = new BufferedReader(new InputStreamReader(in));
             String readline;
             int counter = 0;
