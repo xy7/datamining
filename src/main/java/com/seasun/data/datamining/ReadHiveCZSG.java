07ca9c91915d49d60afff0e8e97e89b69e68a952
wangxiaoyang <wangxiaoyang@kingsoft.com>
2016-04-15 22:33:55 +0800

    1

--- src/main/java/com/seasun/data/datamining/ReadHiveCZSG.java
+++ src/main/java/com/seasun/data/datamining/ReadHiveCZSG.java
@@ -28,13 +28,10 @@ import org.apache.mahout.math.Vector;
 public class ReadHiveCZSG {
 	private static final int TARGET_AFTTER_DAYS = 30;
 
-	private static int RETAIN_THRESHOLD = 15;
-
 	private static String APPID = "1024appid";
 
 	private static PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out, Charsets.UTF_8), true);
 
-	private static Map<LocalDate, Map<String, Integer>> lost = new HashMap<>();
 	private static OnlineLogisticRegression lr;
 	private static int numFeatures;
 
@@ -69,14 +66,13 @@ public class ReadHiveCZSG {
 	private static int SCORE_FREQ;
 
 	private static Map<LocalDate, Map<String, Map<String, Integer>>> ldAccountMaps = new HashMap<>();
-	
+
 	public static void main(String[] args) throws Exception {
 
 		Utils.loadConfigFile("./config.properties.hive");
 		APPID = Utils.getOrDefault("appid", APPID);
 		SCORE_FREQ = Utils.getOrDefault("score_freq", 0);
 		MAY_LOST_REPEAT_CNT = Utils.getOrDefault("may_lost_repeat_cnt", 1);
-		RETAIN_THRESHOLD = Utils.getOrDefault("retain_threshold", 15);
 
 		LocalDate start = LocalDate.parse(Utils.getOrDefault("train_start", "2015-11-01"));
 		LocalDate end = LocalDate.parse(Utils.getOrDefault("train_end", "2015-11-01"));
@@ -84,9 +80,9 @@ public class ReadHiveCZSG {
 		LocalDate evalStart = LocalDate.parse(Utils.getOrDefault("eval_start", "2015-11-01"));
 		LocalDate evalEnd = LocalDate.parse(Utils.getOrDefault("eval_end", "2015-11-01"));
 
-		//new code 
-		//step1/3, load all of hive data to map, write to local file
-		//   if exits local file, load to map
+		// new code
+		// step1/3, load all of hive data to map, write to local file
+		// if exits local file, load to map
 		String serialFileName = "./serial_data_" + APPID + ".out";
 		File file = new File(serialFileName);
 		if (!file.exists()) {
@@ -98,13 +94,13 @@ public class ReadHiveCZSG {
 			ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(file));
 			out.writeObject(ldAccountMaps);
 			out.close();
-		} else{
+		} else {
 			out.println("serial file exists read to Map");
 			ObjectInputStream in = new ObjectInputStream(new FileInputStream(file));
 			ldAccountMaps = (Map<LocalDate, Map<String, Map<String, Integer>>>) in.readObject();
 			in.close();
 		}
-		//step2/3, train data
+		// step2/3, train data
 		int trainPass = Utils.getOrDefault("train_pass", 1);
 		for (int i = 0; i < trainPass; i++) {
 			out.printf(Locale.ENGLISH, "--------pass: %3d ---------%n", i);
@@ -113,8 +109,8 @@ public class ReadHiveCZSG {
 			}
 		}
 		Utils.saveModel(lr);
-		
-		//step3/3, eval data
+
+		// step3/3, eval data
 		out.println("eval sample");
 		for (LocalDate ld = start; !ld.isAfter(end); ld = ld.plusDays(1)) {
 			eval(ld);
@@ -124,36 +120,36 @@ public class ReadHiveCZSG {
 			eval(ld);
 		}
 	}
-	
-	//1, load all of hive data to map, write to local file
-	private static void loadAllHiveData(LocalDate ld1, LocalDate ld2){
+
+	// 1, load all of hive data to map, write to local file
+	private static void loadAllHiveData(LocalDate ld1, LocalDate ld2) {
 		for (LocalDate ld = ld1; !ld.isAfter(ld2); ld = ld.plusDays(1)) {
 			out.println("load hive data: " + ld.toString());
-			if(ldAccountMaps.containsKey(ld))
+			if (ldAccountMaps.containsKey(ld))
 				continue;
-			
+
 			RemoteIterator<LocatedFileStatus> lfss = Utils.listHdfsFiles(ld);
 			if (lfss == null)
 				return;
-			
+
 			int[] res = { 0, 0, 0 };
-	
+
 			Map<String, Map<String, Integer>> accountMaps = new HashMap<>();
-			
+
 			final LocalDate ldFinal = ld;
 			Utils.analysisHdfsFiles(lfss, new LineHandler() {
 				@Override
 				public boolean handle(String line) {
-	
+
 					Map<String, Integer> cols = new HashMap<>();
 					String accountId = lineSplit(line, ldFinal, cols);
 
-					if(accountId == null){
+					if (accountId == null) {
 						res[1]++;
 						return false;
 					}
-					
-					if(cols.size() < 50){
+
+					if (cols.size() < 50) {
 						res[0]++;
 						return false;
 					}
@@ -164,7 +160,7 @@ public class ReadHiveCZSG {
 					return true;
 				}
 			});
-			
+
 			ldAccountMaps.put(ld, accountMaps);
 
 			out.printf(
@@ -175,20 +171,20 @@ public class ReadHiveCZSG {
 
 	private static void eval(LocalDate ld) {
 		out.println("eval: " + ld.toString());
-		Integer[][] res = { {0, 0, 0}, {0, 0, 0}, {0, 0, 0} };// abcd;
-		
+		Integer[][] res = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };// abcd;
+
 		Map<String, Map<String, Integer>> samples = ldAccountMaps.get(ld);
-		for(Map.Entry<String, Map<String, Integer>> sample:samples.entrySet() ){
+		for (Map.Entry<String, Map<String, Integer>> sample : samples.entrySet()) {
 			String accountId = sample.getKey();
 			Vector input = parseColumnMap(sample.getValue(), ld);
-			if(input == null)
+			if (input == null)
 				continue;
-				
+
 			int targetValue = getTargetValue(ld, accountId);
-			if(targetValue < 0)
+			if (targetValue < 0)
 				continue;
-			
-			//classify
+
+			// classify
 			Vector score = lr.classify(input);
 
 			double s1 = score.get(0);
@@ -196,47 +192,48 @@ public class ReadHiveCZSG {
 			double s0 = 1 - s1 - s2;
 
 			int predictValue = 0;
-			if(s1>=Utils.CLASSIFY_VALUE || (s1 >= s2 && s1 >= s0 )){
+			if (s1 >= Utils.CLASSIFY_VALUE || (s1 >= s2 && s1 >= s0)) {
 				predictValue = 1;
 			} else if (s2 > s1 && s2 > s0)
 				predictValue = 2;
-			
-			res[targetValue][predictValue] ++;
+
+			res[targetValue][predictValue]++;
 		}
 
 		int all = 0;
-		for(int i=0;i<3;i++)
-			for(int j=0;j<3;j++)
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 3; j++)
 				all += res[i][j];
-		
+
 		out.printf("result matrix all: %d %n", all);
-		for(int i=0;i<3;i++){
-			for(int j=0;j<3;j++){
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
 				out.printf("%2.4f \t", (double) res[i][j] / all);
 			}
 			out.printf("%n");
 		}
-		
+
 	}
 
 	private static int MAY_LOST_REPEAT_CNT;
+
 	private static void train(LocalDate ld) {
 		out.println("train: " + ld.toString());
 		int sampleCnt = 0;
 		rowstat = new int[] { 0, 0, 0, 0, 0 };
 		Map<String, Map<String, Integer>> samples = ldAccountMaps.get(ld);
-		for(Map.Entry<String, Map<String, Integer>> sample:samples.entrySet() ){
+		for (Map.Entry<String, Map<String, Integer>> sample : samples.entrySet()) {
 			new RandomAccessSparseVector(numFeatures);
 			String accountId = sample.getKey();
 			Vector input = parseColumnMap(sample.getValue(), ld);
-			if(input == null)
+			if (input == null)
 				continue;
-				
+
 			int targetValue = getTargetValue(ld, accountId);
-			if(targetValue < 0)
+			if (targetValue < 0)
 				continue;
-			
-			//print score
+
+			// print score
 			if (SCORE_FREQ != 0 && (++sampleCnt) % SCORE_FREQ == 0) {
 				// check performance while this is still news
 				double logP = lr.logLikelihood(targetValue, input);
@@ -258,10 +255,10 @@ public class ReadHiveCZSG {
 			} else
 				lr.train(targetValue, input);
 		}
-		
+
 		out.printf(
 				"train finish date:%s, first_login_date error: %d, uptodate < 14: %d, last14LoginDaycnt not in[2,13]: %d, getTargetValue error:%d   %n"
-				,ld.toString(), rowstat[1], rowstat[2], rowstat[3], rowstat[0]);
+				, ld.toString(), rowstat[1], rowstat[2], rowstat[3], rowstat[0]);
 
 	}
 
@@ -271,18 +268,18 @@ public class ReadHiveCZSG {
 		String appId = cols[0];
 		if (!appId.equals(APPID))
 			return null;
-		
+
 		String accountId = cols[1];
 
-		//res.put("app_id", cols[0]);
-		//res.put("account_id", cols[1]);
-		//res.put("device_id", cols[2]);
-		//res.put("mac", cols[3]);
+		// res.put("app_id", cols[0]);
+		// res.put("account_id", cols[1]);
+		// res.put("device_id", cols[2]);
+		// res.put("mac", cols[3]);
 
 		String mapStr = cols[4];
 		String mapInt = cols[5];
 		Map<String, String> strMap = parse2map(mapStr);
-		
+
 		String fistLoginDate = strMap.get("first_login_date");
 		if (fistLoginDate == null || fistLoginDate.length() != 10) {
 			// out.println("fistLoginDate format error: " + fistLoginDate);
@@ -292,7 +289,7 @@ public class ReadHiveCZSG {
 		int uptodate = Utils.dateDiff(ld, firstLoginDate);
 		res.put("up_to_date", uptodate);
 
-		//res.putAll(parse2map(mapStr));
+		// res.putAll(parse2map(mapStr));
 		res.putAll(parse2mapint(mapInt));
 
 		return accountId;
@@ -307,19 +304,19 @@ public class ReadHiveCZSG {
 			String value = "\\N";
 			if (kv.length >= 2)
 				value = kv[1];
-			
+
 			res.put(kv[0], value);
 		}
 		return res;
 	}
-	
+
 	private static Map<String, Integer> parse2mapint(String mapInt) {
 		Map<String, String> map = parse2map(mapInt);
 		Map<String, Integer> res = new HashMap<>();
-		for(String k:map.keySet()){
+		for (String k : map.keySet()) {
 			if (map.get(k).equals("\\N"))// null值替换为0
 				res.put(k, 0);
-			else 
+			else
 				res.put(k, Integer.parseInt(map.get(k)));
 		}
 
@@ -327,37 +324,37 @@ public class ReadHiveCZSG {
 	}
 
 	private static int[] rowstat;
-	
+
 	private static int getTargetValue(LocalDate ld, String accountId) {
-		
+
 		try {
 			Map<String, Integer> cols = ldAccountMaps.get(ld.plusDays(TARGET_AFTTER_DAYS)).get(accountId);
-			if (cols == null){
+			if (cols == null) {
 				rowstat[0]++;
 				return -1;
 			}
-		
 
 			int lastAllLoginDaycnt = cols.get("last30_login_daycnt");
 			int lastHalfLoginDaycnt = cols.get("last14_login_daycnt");
 			int nextHalfLoginDaycnt = lastAllLoginDaycnt - lastHalfLoginDaycnt;
-			//int last30LoginDaycnt = Integer.parseInt(cols.get("last30_login_daycnt"));
+			// int last30LoginDaycnt =
+			// Integer.parseInt(cols.get("last30_login_daycnt"));
 
 			int targetValue = 1;// 将流失用户
 			if (nextHalfLoginDaycnt > 0 && lastHalfLoginDaycnt > 0) {// 留存用户
 				targetValue = 2;
-			} else if (nextHalfLoginDaycnt == 0 ) {// 流失用户
+			} else if (nextHalfLoginDaycnt == 0) {// 流失用户
 				targetValue = 0;
 			}
 
 			return targetValue;
 		} catch (Exception e) {
 			// e.printStackTrace();
-			//out.println(e);
+			// out.println(e);
 			rowstat[0]++;
 			return -2;
 		}
-	
+
 	}
 
 	// 返回帐号id
@@ -369,9 +366,9 @@ public class ReadHiveCZSG {
 		int i = 1;
 		for (String k : trainIndex) {
 			Integer s = cols.get(k);
-//			if (s.equals("\\N"))// null值替换为0
-//				s = "0";
-			featureVector.setQuick(i, (double)s);
+			// if (s.equals("\\N"))// null值替换为0
+			// s = "0";
+			featureVector.setQuick(i, (double) s);
 			i++;
 		}
 
